a3429cd507f415238319c8ed9adb9aad83f040b7
Shizuka <58659139+Shizu-ka@users.noreply.github.com>
2023-03-20 14:13:11 +0700

    Update Footer.tsx

--- components/common/Footer/Footer.tsx
+++ components/common/Footer/Footer.tsx
@@ -3,10 +3,10 @@ import Image from "next/image";
 export function Footer() {
   return (
     <a
-      href="https://github.com/debbyalmadea/PDF-Merger"
+      href="http://shizuka.my.id/"
       className="flex flex-row justify-center items-center lg:space-x-4 space-x-2"
     >
-      <p className="lg:text-lg text-xs">Open Source</p>
+      <p className="lg:text-lg text-xs">Thanks to debbyalmadea | © Shizuka 2023</p>
       <div className="lg:w-6 w-4 lg:h-6 h-4 relative">
         <Image src="/github-logo.png" alt="github-logo" fill />
       </div>
