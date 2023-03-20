16f3b174835946daedfa2c22d0761ec85203e7b0
Shizuka <58659139+Shizu-ka@users.noreply.github.com>
2023-03-20 14:18:46 +0700

    Update Footer.tsx

--- components/common/Footer/Footer.tsx
+++ components/common/Footer/Footer.tsx
@@ -3,8 +3,9 @@ import Image from "next/image";
 export function Footer() {
   return (
     <a
-      href="http://shizuka.my.id/" target="_blank"
+      href="http://shizuka.my.id/"
       className="flex flex-row justify-center items-center lg:space-x-4 space-x-2"
+      target="_blank"
     >
       <p className="lg:text-lg text-xs">Thanks to debbyalmadea | © Shizuka 2023</p>
       <div className="lg:w-6 w-4 lg:h-6 h-4 relative">
