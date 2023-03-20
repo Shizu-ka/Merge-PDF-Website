537f3c80eb14f37101b45a265c1eb027cc9426c5
Shizuka <58659139+Shizu-ka@users.noreply.github.com>
2023-03-20 18:55:00 +0700

    Update PDFMerger.tsx

--- components/pdfmerger/components/PDFMerger/PDFMerger.tsx
+++ components/pdfmerger/components/PDFMerger/PDFMerger.tsx
@@ -201,7 +201,7 @@ export default function PDFMerger() {
       <p className="sm:text-2xl text-lg text-center mt-4 mb-8">
         Merge PDFs and Images quickly and securely
       </p>
-      <p className="sm:text-2xl text-md text-center mt-4 mb-8">
+      <p className="sm:text-2xl text-sm text-center mt-1 mb-8">
         Your Privacy is Secure, Because ShizPDF is Serverless      
       </p>
       <div className="flex flex-col justify-center items-center w-[320px] sm:w-[560px]">
